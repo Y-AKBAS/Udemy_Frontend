05b6ace63e7fbe67ae21913c55f0fb1ed4245015
yasin <yas.akbass@gmail.com>
2024-02-26 22:26:20 +0100

    update pokemon append

--- dom/append.js
+++ dom/append.js
@@ -1,14 +1,14 @@
 
 const pokemonContainer = document.querySelector('#container');
 
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 151; i++) {
     const imageDiv = document.createElement('div');
-    imageDiv.classList.add()
+    imageDiv.classList.add('pokemon');
     const label = document.createElement('span');
     label.innerText = `#${i}`;
     const image = document.createElement('img');
     image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
-    imageDiv.appendChild(label);
     imageDiv.appendChild(image);
+    imageDiv.appendChild(label);
     pokemonContainer.appendChild(imageDiv)
 }
