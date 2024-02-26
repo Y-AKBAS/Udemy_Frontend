d11bd4308cdf9eeab472bf431e3e6cf1c78e91a6
Y-AKBAS <yas.akbass@gmail.com>
2024-02-26 21:23:15 +0000

    update url

--- dom/append.js
+++ dom/append.js
@@ -1,14 +1,13 @@
-// https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/1.png
 
 const pokemonContainer = document.querySelector('#container');
 
-for (let i = 1; i <= 2; i++) {
+for (let i = 1; i <= 5; i++) {
     const imageDiv = document.createElement('div');
+    imageDiv.classList.add()
     const label = document.createElement('span');
     label.innerText = `#${i}`;
-    //imageDiv.classList.add("append.css");
     const image = document.createElement('img');
-    image.src = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${i}.png`
+    image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
     imageDiv.appendChild(label);
     imageDiv.appendChild(image);
     pokemonContainer.appendChild(imageDiv)
