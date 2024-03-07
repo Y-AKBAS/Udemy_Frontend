4efb8ed5c52242ff827aba6780b33421a9dd14db
Y-AKBAS <yas.akbass@gmail.com>
2024-03-07 20:23:02 +0000

    search

---
+++ javascript-part/tvShowSearchPage/search.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const searchTerm = form.elements.searchTerm.value;
+    const shows = await searchFor(searchTerm);
+    addImages(shows);
+});
+
+const searchFor = async (searchTerm) => {
+    const url = `http://api.tvmaze.com/search/shows?q=${searchTerm}`
+    console.log(url);
+    const response = await fetch(url)
+    const body = await response.json();
+    return body;
+};
+
+const addImages = (shows) => {
+    const showsWithImages = shows.filter(elem => elem.show.image);
+    for (let elem of showsWithImages) {
+        const img = document.createElement('IMG');
+        img.src = elem.show.image.medium;
+        document.body.append(img);
+    }
+}
