3013403e51e76ccb4cf77fb78046b4f6df330983
Y-AKBAS <yas.akbass@gmail.com>
2024-03-14 12:15:50 +0000

    add first react project

---
+++ react/pokemon/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi There!
+      </header>
+    </div>
+  );
+}
+
+export default App;
